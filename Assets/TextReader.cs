d66d0db602ccd1649cee890b5acd1137a8a97e49
Titian <httc805155@gmail.com>
2021-07-25 22:04:53 +0800

    Add UniVRM and vroid character

---
+++ Assets/TextReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class TextReader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadString("Assets/Resources/model.txt");
+    }
+
+    static void ReadString(string path)
+    {
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        var list = reader.ReadToEnd().Split('\n').Select(Convert.ToSingle).ToList();
+
+        Debug.Log(list);
+        reader.Close();
+    }
+}
