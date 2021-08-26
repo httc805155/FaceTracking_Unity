3aa6897643695654cd3a9f819a4a63d0428aa4e0
DESKTOP-E6TMLH3\RD <titian@fengshui-vision.com>
2021-08-26 17:26:44 +0800

    Testing commit

---
+++ Assets/MediaPipeTest.cs
@@ -0,0 +1,215 @@
+﻿using Mediapipe;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediaPipeTest : MonoBehaviour
+{
+    [SerializeField] bool useGPU = true;
+
+    [SerializeField]
+    private DemoGraph graph;
+    [SerializeField]
+    private WebCamScreenController webcamScreenController;
+
+    const int MAX_WAIT_FRAME = 1000;
+    private GpuResources gpuResources;
+    private GlCalculatorHelper gpuHelper;
+    static IntPtr currentContext = IntPtr.Zero;
+
+    private Coroutine graphRunner;
+    private Coroutine cameraSetupCoroutine;
+    private WebCamDevice? webCamDevice;
+    private object graphLock = new object ();
+
+    void Start()
+    {
+#if UNITY_EDITOR_OSX || UNITY_EDITOR_WIN
+#if UNITY_STANDALONE
+        if (useGPU)
+        {
+            Debug.LogWarning("PC Standalone on macOS or Windows does not support GPU. Uncheck `Use GPU` from the Inspector window (SceneDirector).");
+        }
+#endif
+#endif
+
+#if UNITY_ANDROID && !UNITY_EDITOR_OSX && !UNITY_EDITOR_WIN
+    if (IsGpuEnabled()) {
+      PluginCallback callback = GetCurrentContext;
+
+      var fp = Marshal.GetFunctionPointerForDelegate(callback);
+      GL.IssuePluginEvent(fp, 1);
+    }
+#endif
+        var resourceManager = GameObject.Find("ResourceManager");
+#if UNITY_EDITOR
+        resourceManager.AddComponent<LocalAssetLoader>();
+#else
+    resourceManager.AddComponent<AssetBundleLoader>();
+#endif
+        var webcamDevice = GetDefaultWebcam();
+        if (webcamDevice.HasValue)
+        {
+            ChangeWebCamDevice(webcamDevice);
+        }
+    }
+
+    private WebCamDevice? GetDefaultWebcam()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        // for debugging purposes, prints available devices to the console
+        for (int i = 0; i < devices.Length; i++)
+        {
+            print("Webcam available: " + devices[i].name);
+        }
+        if (devices.Length > 0)
+        {
+            return devices[0];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void ChangeWebCamDevice(WebCamDevice? webCamDevice)
+    {
+        lock (graphLock)
+        {
+            ResetCamera(webCamDevice);
+
+            if (graph != null)
+            {
+                StopGraph();
+                StartGraph();
+            }
+        }
+    }
+
+    void ResetCamera(WebCamDevice? webCamDevice)
+    {
+        StopCamera();
+        cameraSetupCoroutine = StartCoroutine(webcamScreenController.ResetScreen(webCamDevice));
+        this.webCamDevice = webCamDevice;
+    }
+
+    void StopCamera()
+    {
+        if (cameraSetupCoroutine != null)
+        {
+            StopCoroutine(cameraSetupCoroutine);
+            cameraSetupCoroutine = null;
+        }
+    }
+
+    void StartGraph()
+    {
+        if (graphRunner != null)
+        {
+            Debug.Log("The graph is already running");
+            return;
+        }
+
+        if (IsGpuEnabled())
+        {
+            SetupGpuResources();
+        }
+        graphRunner = StartCoroutine(RunGraph());
+    }
+
+    void StopGraph()
+    {
+        if (graphRunner != null)
+        {
+            StopCoroutine(graphRunner);
+            graphRunner = null;
+        }
+    }
+
+    void SetupGpuResources()
+    {
+        if (gpuResources != null)
+        {
+            Debug.Log("Gpu resources are already initialized");
+            return;
+        }
+
+        // TODO: have to wait for currentContext to be initialized.
+        if (currentContext == IntPtr.Zero)
+        {
+            Debug.LogWarning("No EGL Context Found");
+        }
+        else
+        {
+            Debug.Log($"EGL Context Found ({currentContext})");
+        }
+
+        gpuResources = GpuResources.Create(currentContext).Value();
+        gpuHelper = new GlCalculatorHelper();
+        gpuHelper.InitializeForTest(gpuResources);
+    }
+
+    IEnumerator RunGraph()
+    {
+        yield return WaitForCamera(webcamScreenController);
+
+        if (!webcamScreenController.isPlaying)
+        {
+            Debug.LogWarning("WebCamDevice is not working. Stopping...");
+            yield break;
+        }
+
+        if (IsGpuEnabled())
+        {
+            graph.Initialize(gpuResources, gpuHelper);
+        }
+        else
+        {
+            graph.Initialize();
+        }
+
+        graph.StartRun(webcamScreenController.GetScreen()).AssertOk();
+
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+
+            var nextFrameRequest = webcamScreenController.RequestNextFrame();
+            yield return nextFrameRequest;
+
+            var nextFrame = nextFrameRequest.textureFrame;
+
+            graph.PushInput(nextFrame).AssertOk();
+            graph.RenderOutput(webcamScreenController, nextFrame);
+        }
+    }
+
+    IEnumerator WaitForCamera(WebCamScreenController webCamScreenController)
+    {
+        var waitFrame = MAX_WAIT_FRAME;
+
+        yield return new WaitUntil(() =>
+        {
+            waitFrame--;
+            var isWebCamPlaying = webCamScreenController.isPlaying;
+
+            if (!isWebCamPlaying && waitFrame % 50 == 0)
+            {
+                Debug.Log($"Waiting for a WebCamDevice");
+            }
+
+            return isWebCamPlaying || waitFrame < 0;
+        });
+    }
+
+    bool IsGpuEnabled()
+    {
+#if UNITY_EDITOR_OSX || UNITY_EDITOR_WIN
+        return false;
+#else
+    return useGPU;
+#endif
+    }
+}
--- Assets/TextReader.cs
+++ Assets/TextReader.cs
@@ -7,19 +7,56 @@ using UnityEngine;
 
 public class TextReader : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject indicator_red;
+
+    [SerializeField]
+    private GameObject indicator_blue;
+
     // Start is called before the first frame update
     void Start()
     {
-        ReadString("Assets/Resources/model.txt");
+        var headmodel = ReadString("Assets/Resources/model.txt");
+
+        foreach (var point in headmodel)
+        {
+            var go = GameObject.Instantiate(indicator_red, new Vector3(point.x, -point.y, point.z), Quaternion.identity);
+            go.SetActive(true);
+        }
+
+        var facemodel = new List<Vector3> 
+        {
+            new Vector3(0.0f, 0.0f, 0.0f),
+            new Vector3(0.0f, -330f, -65.0f),
+            new Vector3(-225.0f, 170.0f, -135.0f),     // Left eye left corner
+            new Vector3(225.0f, 170.0f, -135.0f),      // Right eye right corner
+            new Vector3(-150.0f, -150.0f, -125.0f),    // Mouth left corner
+            new Vector3(150.0f, -150.0f, -125.0f)
+        };
+
+        foreach (var point in facemodel)
+        {
+            var go = GameObject.Instantiate(indicator_blue, point / 4.5f, Quaternion.identity);
+            go.SetActive(true);
+        }
     }
 
-    static void ReadString(string path)
+    private List<Vector3> ReadString(string path)
     {
+        List<Vector3> headModel = new List<Vector3>();
+
         //Read the text from directly from the test.txt file
         StreamReader reader = new StreamReader(path);
         var list = reader.ReadToEnd().Split('\n').Select(Convert.ToSingle).ToList();
 
-        Debug.Log(list);
+
+        var step = list.Count / 3;
+        for (var i = 0; i < step; i++)
+        {
+            var vector = new Vector3(list[i], list[i + step*1], list[i + step*2]);
+            headModel.Add(vector);
+        }
         reader.Close();
+        return headModel;
     }
 }
